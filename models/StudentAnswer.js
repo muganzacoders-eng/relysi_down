55bb233f0b216f8fabf02e2b8b885009d79bf136
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 20:36:35 +0300

    Update files

---
+++ models/StudentAnswer.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataTypes) => {
+const StudentAnswer = sequelize.define('StudentAnswer', {
+  answer_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  attempt_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  question_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  answer: {
+    type: DataTypes.TEXT
+  },
+  is_correct: {
+    type: DataTypes.BOOLEAN
+  },
+  marks_awarded: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  tableName: 'student_answers',
+  timestamps: false
+});
+
+StudentAnswer.associate = (models) => {
+  StudentAnswer.belongsTo(models.ExamAttempt, { foreignKey: 'attempt_id' });
+  StudentAnswer.belongsTo(models.Question, { foreignKey: 'question_id' });
+};
+
+ return StudentAnswer;
+};
