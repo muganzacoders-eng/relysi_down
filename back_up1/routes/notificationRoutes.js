16284f8cfa08c67f3f3902c81aa6fed68242e075
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 20:32:36 +0300

    Update files

---
+++ back_up1/routes/notificationRoutes.js
@@ -0,0 +1,10 @@
+// routes/notificationRoutes.js
+const express = require('express');
+const router = express.Router();
+const notificationController = require('../controllers/notificationController');
+const auth = require('../middleware/auth');
+
+router.get('/', auth, notificationController.getNotifications);
+router.put('/:id/read', auth, notificationController.markAsRead);
+
+module.exports = router;
