d84b1d45616096d07bc7d9dd8ef7905b7c87755d
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 20:32:00 +0300

    Update files

---
+++ back_up1/models/SessionReport.js
@@ -0,0 +1,42 @@
+// const { DataTypes } = require('sequelize');
+// const { sequelize } = require('../config/db');
+
+module.exports = (sequelize, DataTypes) => {
+const SessionReport = sequelize.define('SessionReport', {
+  report_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  session_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  expert_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  report_text: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  recommendations: {
+    type: DataTypes.TEXT
+  },
+  submitted_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'session_reports',
+  timestamps: false
+});
+
+SessionReport.associate = (models) => {
+  SessionReport.belongsTo(models.CounselingSession, { foreignKey: 'session_id' });
+  SessionReport.belongsTo(models.User, { foreignKey: 'expert_id' });
+};
+
+// module.exports = SessionReport;
+ return SessionReport;
+};
