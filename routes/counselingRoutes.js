c6eb4a9943886c594fd798798335113959a5f7b4
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 20:37:09 +0300

    Update files

---
+++ routes/counselingRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const counselingController = require('../controllers/counselingController');
+const auth = require('../middleware/auth');
+
+router.post('/', auth, counselingController.requestSession);
+router.get('/', auth, counselingController.getSessions);
+router.get('/:id', auth, counselingController.getSessionById);
+router.put('/:id', auth, counselingController.updateSession);
+router.post('/:id/confirm', auth, counselingController.confirmSession);
+router.post('/:id/complete', auth, counselingController.completeSession);
+router.post('/:id/confirm-with-meet', auth, counselingController.confirmSessionWithMeet);
+
+module.exports = router;
