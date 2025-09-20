50886507ca6bf362c6f9e0cb6a56cff962264aa5
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 20:31:25 +0300

    Update files

---
+++ back_up1/models/ClassroomEnrollment.js
@@ -0,0 +1,39 @@
+// const { DataTypes } = require('sequelize');
+// const { sequelize } = require('../config/db');
+
+module.exports = (sequelize, DataTypes) => {
+const ClassroomEnrollment = sequelize.define('ClassroomEnrollment', {
+  enrollment_id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  classroom_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  student_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  enrollment_date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'completed', 'dropped'),
+    defaultValue: 'active'
+  }
+}, {
+  tableName: 'classroom_enrollments',
+  timestamps: false
+});
+
+ClassroomEnrollment.associate = (models) => {
+  ClassroomEnrollment.belongsTo(models.Classroom, { foreignKey: 'classroom_id' });
+  ClassroomEnrollment.belongsTo(models.User, { foreignKey: 'student_id' });
+};
+
+// module.exports = ClassroomEnrollment;
+ return ClassroomEnrollment;
+};
