6919f700ef47de6cc6007a45a1d3438478d62c84
muganzacoders-eng <muganzacoders@gmail.com>
2025-09-20 22:46:52 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
       origin: [
+      'https://relysi.com',
+      'https://relysi-on.vercel.app',
       'http://localhost:3000',
       'https://2f0916d7f2d3.ngrok-free.app'
     ],
@@ -26,4 +28,5 @@ server.listen(PORT, () => {
 process.on('unhandledRejection', (err) => {
   console.error('Unhandled Rejection:', err);
   server.close(() => process.exit(1));
+
 });
